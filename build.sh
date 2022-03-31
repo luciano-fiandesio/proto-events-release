697eb19f7707a897e03a954b96f310ee2a660ac9
Luciano Fiandesio <luciano@fiandes.io>
2022-03-31 12:01:25 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,36 @@
 #!/usr/bin/env bash
 
+#
+# Automate the generation of jar files from Protobuf schema definitions
+#
+# This script is meant to be used in a Continuous Integration pipeline.
+# It uses git tags to select the service for which to trigger the protoc code generation.
+# After the code generation, it packaged the classes into a jar.
+# The jar is named after the area and service name: area-my-service-1.2.3.jar
+#
+# Usage: ./build.sh <tag>
+#
+# Example: ./build.sh product/my-service/release/1.2.3
+#
+# The -d/--debug flag allows to use the "test" folder for testing and debugging purposes
+# 
+# Example: ./build.sh -d test/my-test-service/release/1.2.3
+#
+# author: Luciano Fiandesio <luciano@fiandes.io>
+#
+
 set -Eeuo pipefail
 trap cleanup SIGINT SIGTERM ERR EXIT
 
 script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 
 SEMVER_REGEX="^(0|[1-9][0-9]*)\\.(0|[1-9][0-9]*)\\.(0|[1-9][0-9]*)(\\-[0-9A-Za-z-]+(\\.[0-9A-Za-z-]+)*)?(\\+[0-9A-Za-z-]+(\\.[0-9A-Za-z-]+)*)?$"
+VALID_AREAS=("user" "product" "finance" "adv")
 
 main() {
 
   # parse tag
-  # expected format: [service-name]/release/[version]
+  # expected format: [area]/[service-name]/release/[version]
 
   tag="${args[0]}"
   dist="dist/generated"
@@ -20,21 +40,22 @@ main() {
 
   IFS='/' read -a arr <<< "$tag"
   
-  service_name=${arr[0]}
-  version=${arr[2]}
+  area=${arr[0]}
+  service_name=${arr[1]}
+  version=${arr[3]}
 
-  FILES=$(find proto/$service_name -maxdepth 1 -name "*.proto")
+  FILES=$(find proto/$area/$service_name -maxdepth 1 -name "*.proto")
 
   for proto in "$FILES"; do
       #echo "> $proto";
       #echo "Running in $script_dir/proto/$service_name/"
       protoc \
         -I proto/include \
-        --proto_path="$script_dir"/proto/"$service_name" \
+        --proto_path="$script_dir"/proto/"$area"/"$service_name" \
         --java_out=dist/generated \
         "$script_dir"/"$proto"
   done
-  _jar=$service_name-events-${version}.jar
+  _jar=$area-$service_name-events-${version}.jar
   # package generated files into jar
   jar cf $_jar "$dist" 
 
@@ -45,19 +66,29 @@ validate_tag() {
   tag=$1
   IFS='/' read -a arr <<< "$tag"
 
-  # check tag contains only three parts
-  if ! [[ ${#arr[@]} -eq 3 ]]; then
+  # check tag contains 4 parts
+  if ! [[ ${#arr[@]} -eq 4 ]]; then
     die "The tag ${RED}$tag${NOFORMAT} has an invalid format."
   fi  
   
-  service=${arr[0]}
-  release=${arr[1]}
-  version=${arr[2]}
+  area=${arr[0]}
+  service=${arr[1]}
+  release=${arr[2]}
+  version=${arr[3]}
 
   # validate service name
   # check that a folder with service name exists
-  if ! [[ -d "$script_dir/proto/$service" ]]; then
-    die "no service found with name: $service"
+  if ! [[ -d "$script_dir/proto/$area/$service" ]]; then
+    die "no service found with name: $area/$service"
+  fi
+
+  if [[ $debug == 1  ]]; then
+    VALID_AREAS+=("test")
+  fi
+
+  # validate area name
+  if [[ ! " ${VALID_AREAS[*]} " =~ " ${area} " ]]; then
+    die "invalid area name: $area"
   fi
 
   # validate second part of tag
@@ -103,13 +134,13 @@ die() {
 
 parse_params() {
   # default values of variables set from params
-  flag=0
-  param=''
-
+  debug=0
+  
   while :; do
     case "${1-}" in
     -v | --verbose) set -x ;;
     --no-color) NO_COLOR=1 ;;
+    -d | --debug) debug=1 ;; # debug flag
     -?*) die "Unknown option: $1" ;;
     *) break ;;
     esac
